5eb982e1ca33a76cf5a42bd58f4041f3135d1e60
ecreyes <ereyesoyarzo@gmail.com>
2019-04-28 15:48:49 -0400

    inicio del proyecto

---
+++ servicio-noticias/src/main/java/com/tps/msnoticias/MsnoticiasApplication.java
@@ -0,0 +1,13 @@
+package com.tps.msnoticias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsnoticiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsnoticiasApplication.class, args);
+	}
+
+}
