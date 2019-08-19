db07757a0c265350638cec96225c19bbf88f6681
ecreyes <ereyesoyarzo@gmail.com>
2019-08-19 16:35:20 -0400

    documentación de apigateway

--- apigateway/src/main/java/com/ts/apigateway/controlador/UsuarioController.java
+++ apigateway/src/main/java/com/ts/apigateway/controlador/UsuarioController.java
@@ -5,37 +5,43 @@ import com.ts.apigateway.modelo.Usuario;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
+/**Controlador de Usuario para la receción de peticiones**/
 @RestController
 public class UsuarioController {
 
   private final UsuarioMsg usuarioMsg;
-
+  /**Nombre de las colas que estan en Rabbit para el envio de mensajes**/
   private static final String ROUTE_KEY_CREATE = "usuario.crear";
   private static final String ROUTE_KEY_DELETE = "usuario.eliminar";
   private static final String ROUTE_KEY_EDIT = "usuario.editar";
 
+  /**Asignación del adaptador de usuario**/
   public UsuarioController(@Qualifier("usuarioMsgAdapter") UsuarioMsg usuarioMsg) {
     this.usuarioMsg = usuarioMsg;
   }
 
+  /**Agregar un usuario**/
   @PostMapping("/usuario/agregar")
   public Usuario agregar(@RequestBody Usuario usuario) {
     usuarioMsg.enviarMsg(usuario, ROUTE_KEY_CREATE);
     return usuario;
   }
 
+  /**Actualizar un usuario**/
   @PutMapping("/usuario/editar")
   public Usuario editar(@RequestBody Usuario usuario) {
     usuarioMsg.enviarMsg(usuario, ROUTE_KEY_EDIT);
     return usuario;
   }
 
+  /**Eliminar un usuario**/
   @DeleteMapping("/usuario/eliminar")
   public Usuario eliminar(@RequestBody Usuario usuario) {
     usuarioMsg.enviarMsg(usuario, ROUTE_KEY_DELETE);
     return usuario;
   }
 
+  /**Login de usuario**/
   @PostMapping("/usuario/login")
   public String login(@RequestBody Usuario usuario) {
     return usuarioMsg.solicitarLogin(usuario);
