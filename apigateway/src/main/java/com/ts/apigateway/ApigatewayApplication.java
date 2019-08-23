57d192923751b3d9b6ac4305f87374bdd42eb24e
Felipe Gonzalez Alarcon <felipe.gonzalezalarcon94@gmail.com>
2019-08-22 21:38:20 -0400

    Bcrypt para hash de password de usuarios
    Signed-off-by: Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>

--- apigateway/src/main/java/com/ts/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/ts/apigateway/ApigatewayApplication.java
@@ -2,11 +2,13 @@ package com.ts.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
 /*
 Clase encargada de iniciar el servidor,
 esta clase es propia de Spring Boot
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ApigatewayApplication {
 
   public static void main(String[] args) {
--- apigateway/src/main/java/com/ts/apigateway/controlador/UsuarioController.java
+++ apigateway/src/main/java/com/ts/apigateway/controlador/UsuarioController.java
@@ -3,47 +3,71 @@ package com.ts.apigateway.controlador;
 import com.ts.apigateway.mensajeria.UsuarioMsg;
 import com.ts.apigateway.modelo.Usuario;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-/**Controlador de Usuario para la receción de peticiones**/
+/**
+ * Controlador de Usuario para la receción de peticiones
+ **/
 @RestController
 public class UsuarioController {
 
   private final UsuarioMsg usuarioMsg;
-  /**Nombre de las colas que estan en Rabbit para el envio de mensajes**/
+  //Rutas utilizadas para enviar mensajes hacia colas receptoras
   private static final String ROUTE_KEY_CREATE = "usuario.crear";
   private static final String ROUTE_KEY_DELETE = "usuario.eliminar";
   private static final String ROUTE_KEY_EDIT = "usuario.editar";
 
-  /**Asignación del adaptador de usuario**/
+  //BCrypt Hash
+  private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(15);
+
+  /**
+   * Asignación del adaptador de usuario
+   **/
   public UsuarioController(@Qualifier("usuarioMsgAdapter") UsuarioMsg usuarioMsg) {
     this.usuarioMsg = usuarioMsg;
   }
 
-  /**Agregar un usuario**/
+  /**
+   * Agregar un usuario
+   **/
   @PostMapping("/usuario/agregar")
   public Usuario agregar(@RequestBody Usuario usuario) {
+    usuario.setPassword(encoder.encode(usuario.getPassword()));
     usuarioMsg.enviarMsg(usuario, ROUTE_KEY_CREATE);
     return usuario;
   }
 
-  /**Actualizar un usuario**/
+  /**
+   * Actualizar un usuario
+   **/
   @PutMapping("/usuario/editar")
   public Usuario editar(@RequestBody Usuario usuario) {
+    usuario.setPassword(encoder.encode(usuario.getPassword()));
     usuarioMsg.enviarMsg(usuario, ROUTE_KEY_EDIT);
     return usuario;
   }
 
-  /**Eliminar un usuario**/
+  /**
+   * Eliminar un usuario
+   **/
   @DeleteMapping("/usuario/eliminar")
   public Usuario eliminar(@RequestBody Usuario usuario) {
     usuarioMsg.enviarMsg(usuario, ROUTE_KEY_DELETE);
     return usuario;
   }
 
-  /**Login de usuario**/
+  /**
+   * Login de usuario
+   **/
   @PostMapping("/usuario/login")
   public String login(@RequestBody Usuario usuario) {
+    //TODO: Determinar si se debe hashear password en cada login (o solo hacer matches en msusuario,
+    // implica enviar password por mensajeria en texto plano)
     return usuarioMsg.solicitarLogin(usuario);
   }
 }
